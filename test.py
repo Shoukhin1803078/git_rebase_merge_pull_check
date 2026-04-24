c25bc42cdc1df46a5163f64de5670fa9baa8d766
Shoukhin1803078 <alamintokdercse@gmail.com>
2026-04-24 12:47:21 +0600

    Update greeting messages in test.py to include multiple branch identifiers

--- test.py
+++ test.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
     num2 = 7
     result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}")
-    print("Hello World")
+    print("Hello this is develop branch")
+    print("Hello this is main branch")
+    print("Hello this is feature branch")
+    print("Hello this is feature branch")
+    print("Hello this is feature branch")   
